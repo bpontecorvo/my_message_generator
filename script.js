011f96520e1ed7917d31640828db4b18d3aab65e
bpontecorvo <blpontecorvo@gmail.com>
2021-10-16 13:56:49 -0700

    Added js object

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 console.log("Hello World");
+
+const spiritWisdom = {
+    spiritAnimal: ['Orca', 'Foster Dog', 'Snek'],
+    spiritPower: ['enhanced smelling abilities', 'confident public speaker', 'never becomes dehydrated'],
+    spiritPlace: ['Outer Space', 'Central Park', 'Spokane'],
+    };
