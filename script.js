6603b588057e4bce852c2d9b855507e741e51c2f
bpontecorvo <blpontecorvo@gmail.com>
2021-10-18 22:30:23 -0700

    minor updates

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 console.log("Hello World");
 
+//Generate random numbers
+function generateRandomNumber (num) {
+    return Math.floor(Math.random() num * 1)
+}
+console.log(generateRandomNumber(34))
+
+//Data to populate messages
 const spiritWisdom = {
     spiritAnimal: ['Orca', 'Foster Dog', 'Snek'],
     spiritPower: ['enhanced smelling abilities', 'confident public speaker', 'never becomes dehydrated'],
     spiritPlace: ['Outer Space', 'Central Park', 'Spokane'],
     };
+
+// Spirit Output Holder
+let spiritMessage = []
+
+
+
