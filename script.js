f0e86e94320b5b036b1f5c46a31ee19fb9e98dd1
bpontecorvo <blpontecorvo@gmail.com>
2021-10-23 11:55:54 -0700

    Added working switch statement, now functions

--- script.js
+++ script.js
@@ -19,9 +19,17 @@ let spiritMessage = []
 for (prop in spiritWisdom){
     let index = generateRandomNumber(spiritWisdom[prop].length);
     switch(prop) {
-    case "spiritAnnimal": 
-    spiritWisdom.push(`Today your spirit animal is $`)
-
+    case "spiritAnimal": 
+        spiritMessage.push(`Today your spirit animal is ${spiritWisdom[prop][index]}`);
+        break
+    case "spiritPower": 
+        spiritMessage.push(`Your spirit power is ${spiritWisdom[prop][index]}`);
+        break
+    case "spiritPlace": 
+        spiritMessage.push(`Lastly, your spiritual place is ${spiritWisdom[prop][index]}`);
+        break
+    default:
+        spiritWisdom.push('Not enough information');
     }
 }
 
