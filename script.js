8bec433eb62054879d65c4d70f1540d63668f508
gpontecorvo <gregoryp@lynden.com>
2021-10-24 22:21:10 -0700

    Read from file

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
-console.log("Hello World");
+var debugIt = false;
+
+var fs=require('fs');
+
+// read data from file
+var jsonData=fs.readFileSync('spiritprops.json', 'utf8');
+if (debugIt) {
+    console.log("SpiritWisdom JSON from file:\n" + jsonData + "\n");
+}
+var spiritWisdom=JSON.parse(jsonData);
+
 
 //Generate random numbers
 function generateRandomNumber (num) {
     return Math.floor(Math.random() * num )
 }
-console.log(generateRandomNumber(34))
-
-//Data to populate messages
-const spiritWisdom = {
-    spiritAnimal: ['Orca', 'Foster Dog', 'Snek'],
-    spiritPower: ['enhanced smelling abilities', 'confident public speaker', 'never becomes dehydrated'],
-    spiritPlace: ['Outer Space', 'Central Park', 'Spokane'],
-    };
 
 // Spirit Output Holder
 let spiritMessage = []
 
 for (prop in spiritWisdom){
     let index = generateRandomNumber(spiritWisdom[prop].length);
+    var now = new Date();
     switch(prop) {
-    case "spiritAnimal": 
-        spiritMessage.push(`Today your spirit animal is ${spiritWisdom[prop][index]}`);
+    case "spiritAnimal":
+        spiritMessage.push(`At ${now.toLocaleTimeString()}  on ${now.toLocaleDateString()}, your spirit animal is ${spiritWisdom[prop][index]}`);
         break
     case "spiritPower": 
         spiritMessage.push(`Your spirit power is ${spiritWisdom[prop][index]}`);
